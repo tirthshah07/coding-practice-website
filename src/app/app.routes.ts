5cf437b88981401b58620c1f47cee1d711ff6e8b
Tirth Shah <shahti42@gmail.com>
2025-03-31 20:47:16 +0530

    intial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '', pathMatch:'full'},
+    {path: '', loadComponent: ()=> import('./pages/home/home.component').then(m => m.HomeComponent)},
+    {path: 'explore', loadComponent: () => import('./pages/explore/explore.component').then(m => m.ExploreComponent)}
+];
